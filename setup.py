7bbf9d69d11a295d457cced30edd29eb7a23c419
Florian Kromer <thinwybk@mailbox.org>
2018-04-19 21:10:59 +0200

    change license in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name="rospbt",
     version="0.1.0",
     url="https://github.com/fkromer/rospbt",
-    license='MIT',
+    license='GPLv3',
     author="Florian Kromer",
     author_email="florian.kromer@mailbox.org",
     description="Property Based Testing for the ROS node level.",
@@ -24,6 +24,7 @@ setup(
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
