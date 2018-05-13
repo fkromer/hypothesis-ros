11f9469f6d9abb21f03f91e94e4dcdada96a1c15
Florian Kromer <thinwybk@mailbox.org>
2018-05-13 19:47:56 +0200

    add parameters bool, int32, string
    quick and dirty copy of message field strategies

--- hypothesis_ros/parameters.py
+++ hypothesis_ros/parameters.py
@@ -13,11 +13,66 @@ from hypothesis.strategies import (
     datetimes,
     defines_strategy
 )
+from hypothesis_ros.message_fields import bool as common_bool
+from hypothesis_ros.message_fields import int32 as common_int32
+from hypothesis_ros.message_fields import string as common_string
+from hypothesis_ros.message_fields import (
+    INT32_MIN_VALUE, INT32_MAX_VALUE,
+    STRING_MIN_SIZE, STRING_MAX_SIZE
+)
+
 
 DATE_MIN_VALUE = datetime.datetime(datetime.MINYEAR, 1, 1, 0, 0)
 """date: Minimal ISO8601 Date value (0001-01-01 00:00:00)."""
 DATE_MAX_VALUE = datetime.datetime(datetime.MAXYEAR, 12, 31, 23, 59)
 """date: Maximal ISO8601 Date value (9999-12-31 23:59:00)."""
+
+
+def bool():
+    """
+    Generate value for ROS parameter type "bool".
+
+    Returns
+    -------
+    hypothesis.strategies.booleans()
+        Strategy with preconfigured default values.
+
+    """
+    return common_bool()
+
+
+def int32(min_value=INT32_MIN_VALUE, max_value=INT32_MAX_VALUE):
+    """
+    Generate value for ROS parameter type "int32".
+
+    Returns
+    -------
+    hypothesis.strategies.integers()
+        Strategy with preconfigured default values.
+
+    """
+    return common_int32(min_value, max_value)
+
+
+def string(min_size=STRING_MIN_SIZE, max_size=STRING_MAX_SIZE):
+    """
+    Generate value for ROS parameter "string".
+
+    Parameters
+    ----------
+    min_size : int
+        Minimal size to generate.
+    max_size : int
+        Maximal size to generate.
+
+    Returns
+    -------
+    hypothesis.strategies.binary()
+        Strategy with preconfigured default values.
+
+    """
+    return common_string(min_size, max_size)
+
 
 @defines_strategy
 def date(min_value=DATE_MIN_VALUE, max_value=DATE_MAX_VALUE):
--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -4,7 +4,31 @@ Unit tests for the generators of parameters.
 """
 
 from hypothesis import given
-from hypothesis_ros.parameters import date, DATE_MIN_VALUE, DATE_MAX_VALUE
+from hypothesis_ros.parameters import (
+    bool,
+    int32,
+    string,
+    date, DATE_MIN_VALUE, DATE_MAX_VALUE
+)
+
+
+@given(bool())
+def test_message_field_bool_works_as_parameter(generated_value):
+    """Verify message field strategy bool is executable via parameter module."""
+    assert generated_value in [False, True]
+
+
+@given(int32())
+def test_message_field_int32_works_as_parameter(generated_value):
+    """Verify message field int32 strategy is executable via parameter module."""
+    assert isinstance(generated_value, int)
+
+
+@given(string())
+def test_message_field_string_works_as_parameter(generated_value):
+    """Verify message field strategy string is executable via parameter module."""
+    assert isinstance(generated_value, basestring)
+
 
 @given(date())
 def test_date_generates_in_range_value_per_default(generated_value):
